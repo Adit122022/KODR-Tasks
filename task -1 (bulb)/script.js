89fec04ce0c00372306282c03a2c110a381d1a26
Adit122022 <adity202212@gmail.com>
2024-11-22 13:33:22 +0530

    changing the color on mouseover of text

--- task -1 (bulb)/script.js
+++ task -1 (bulb)/script.js
@@ -28,3 +28,6 @@ const p = document.querySelector("#print");
 p.addEventListener("mouseover", function(){
     p.style.color = "red";
 })
+p.addEventListener("mouseleave", function(){
+    p.style.color = "black";
+})
