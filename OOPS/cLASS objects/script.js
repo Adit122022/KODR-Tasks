3496213b6781f5fdde794c06af95186c58fdf0f8
Adit122022 <adity202212@gmail.com>
2024-11-25 13:40:03 +0530

    constrcutor knowledge

---
+++ OOPS/cLASS objects/script.js
@@ -0,0 +1,16 @@
+let Tata= {
+    start(){
+    console.log("Tata Car started");
+},
+stop(){
+    console.log("Tata Car stopped");
+}
+}
+// prototype is possible for single behaviour 
+
+// Creating an object using the constructor function
+let obj1 = new Tata();
+obj1.start();
+let obj2 = new Tata();
+obj2.stop();
+let arr = [];
