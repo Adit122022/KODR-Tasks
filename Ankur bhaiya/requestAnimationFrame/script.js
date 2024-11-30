13ff3ff5d14b7b53a0af49670791eb5d8b587b80
Adit122022 <sharmaji742595@gmail.com>
2024-11-30 14:20:19 +0530

    ReuestAnimationFrame by Ankur Bhaiya

---
+++ Ankur bhaiya/requestAnimationFrame/script.js
@@ -0,0 +1,16 @@
+//  overview about requestAnimationFrame(){}
+// const div =document.createElement("div")
+// const root =document.querySelector("#root")
+// let count = 0
+// function render(){
+// root.innerHTML = ""
+// root.appendChild(div)
+// div.innerHTML = `hello world ${count}`
+// requestAnimationFrame(()=>{
+//      render();
+//  })
+// }
+// render();
+// // jab agla frame aane vala hoga us se just phele is funcntion ko update kar dena  - requestAnimationFrame()
+// function raf(){
+// }
