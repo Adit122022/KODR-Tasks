60feebf891b91ad2edbb42ea00bf4eada183eb23
Adit122022 <sharmaji742595@gmail.com>
2024-11-30 14:47:54 +0530

    requestanimationframe and setInterval

--- Ankur bhaiya/requestAnimationFrame/script.js
+++ Ankur bhaiya/requestAnimationFrame/script.js
@@ -14,3 +14,29 @@
 // // jab agla frame aane vala hoga us se just phele is funcntion ko update kar dena  - requestAnimationFrame()
 // function raf(){
 // }
+
+
+// set Interval  and requestAnimationFrame
+// const div =document.createElement("div")
+// const root =document.querySelector("#root")
+
+// let count = 0
+// function render(){
+//     root.innerHTML = ""
+//     div.innerHTML = `hello world ${count++}`
+//     div.style.height ="2em"
+//     div.style.background="red"
+//     // div.style.width= `${count}vw`
+  
+//     root.appendChild(div)
+//     requestAnimationFrame(()=>{
+//         console.log(" Rendering")
+//         render();
+//     })
+// }
+// render();
+
+
+// setInterval(()=>{
+// render();   
+// },1000)
