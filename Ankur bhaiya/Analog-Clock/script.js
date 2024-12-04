ba11a7cfc809656e40880d323689547baa425db2
Adit122022 <sharmaji742595@gmail.com>
2024-12-04 16:02:32 +0530

    completed analog clock

--- Ankur bhaiya/Analog-Clock/script.js
+++ Ankur bhaiya/Analog-Clock/script.js
@@ -9,9 +9,10 @@ setInterval(function() {
     let date = new Date()
     let getSeconds = date.getSeconds()
     let getMinutes = date.getMinutes()
-    let getHours = date.getHours()
-    console.log(getSeconds)
-    seconds.style.transform = `rotate(${getSeconds * 6}deg) translate(-50%)`
-    minuts.style.transform = `rotate(${getMinutes * 6}deg) translate(-50%)`
-    hours.style.transform = `rotate(${getHours * 6}deg) translate(-50%)`
+    let getHours = date.getHours() 
+    getHours = (getHours % 12);
+    console.log(getHours)
+    seconds.style.transform = `rotate(${getSeconds * 6}deg) translate(-30%)`
+    minuts.style.transform = `rotate(${getMinutes * 6}deg) translate(-40%)`
+    hours.style.transform = `rotate(${(getHours* 30) + (getMinutes / 60) *30}deg) translate(-40%)`
 }, 1000)
