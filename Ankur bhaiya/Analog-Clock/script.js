c99eb8c7e04bdd70cdd13cb5c5aa93d4094e8bac
Adit122022 <sharmaji742595@gmail.com>
2024-12-04 15:16:27 +0530

    added javascript

--- Ankur bhaiya/Analog-Clock/script.js
+++ Ankur bhaiya/Analog-Clock/script.js
@@ -1,2 +1,17 @@
 
+let minuts = document.querySelector(".minuts")
+let hours = document.querySelector(".hours")
+// alert('hello world')
+let seconds = document.querySelector(".hands .seconds")
 
+
+setInterval(function() {
+    let date = new Date()
+    let getSeconds = date.getSeconds()
+    let getMinutes = date.getMinutes()
+    let getHours = date.getHours()
+    console.log(getSeconds)
+    seconds.style.transform = `rotate(${getSeconds * 6}deg) translate(-50%)`
+    minuts.style.transform = `rotate(${getMinutes * 6}deg) translate(-50%)`
+    hours.style.transform = `rotate(${getHours * 6}deg) translate(-50%)`
+}, 1000)
