6631ecf37e14e922b30bed797ca91f1bc2df1994
Adit122022 <adity202212@gmail.com>
2024-12-06 17:27:43 +0530

    page 1 completed without responsive

--- Sarthak bhaiya/React/Bootcamp/class1/src/components/Page1Components/Text.jsx
+++ Sarthak bhaiya/React/Bootcamp/class1/src/components/Page1Components/Text.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Text = () => {
   return (
     <div className='w-full px-20 '>
-        <p className='text-2xl font-normal w-5/6 '> NiceAtNoon is een fijnschalige design studio voor merken die de hoofden en de harten van hun publiek willen
+        <p className='text-2xl leading-tight font-normal w-5/6 '> NiceAtNoon is een fijnschalige design studio voor merken die de hoofden en de harten van hun publiek willen
 veroveren. Geobsedeerd door de magie van design, geloven we sterk in de kracht van 'story-driven design';
 het geeft jouw unieke merkverhaal geloofwaardigheid, zeggingskracht en een niet te negeren
 aantrekkingskracht. Raak en verbind jouw doelgroepen als nooit tevoren! </p>
