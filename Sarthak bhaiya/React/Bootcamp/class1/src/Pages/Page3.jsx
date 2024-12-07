932ada1747a4085777e24d87192241f6ce848a26
Adit122022 <adity202212@gmail.com>
2024-12-07 13:38:58 +0530

    adding remaining folder

--- Sarthak bhaiya/React/Bootcamp/class1/src/Pages/Page3.jsx
+++ Sarthak bhaiya/React/Bootcamp/class1/src/Pages/Page3.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Section1 from '../components/Page3Components/Section1'
+import Section2 from '../components/Page3Components/Section2'
 
 const Page3 = () => {
   return (
     <div>
         <Section1/>
+        <Section2/>
     </div>
   )
 }
