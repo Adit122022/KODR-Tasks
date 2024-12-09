8bfa241a0635865f7c9b0cc4b91635bf5027fd82
Adit122022 <adity202212@gmail.com>
2024-12-10 02:17:06 +0530

    complete responsive

--- Sarthak bhaiya/React/Bootcamp/class2/src/Components/Card.jsx
+++ Sarthak bhaiya/React/Bootcamp/class2/src/Components/Card.jsx
@@ -2,11 +2,10 @@ import React from 'react'
 
 const Card = ({character}) => {
   return (
-    <div className=' mb-10 md:mb-0 text-white md:w-80 flex-grow md:h-3/4 h-3/5 flex group hover:justify-center hover:items-center  shadow-md  rounded-3xl flex-shrink-0  relative overflow-hidden'>
+    <div className=' mb-10 md:mb-0 text-white md:w-80 flex-grow  md:h-[70vh] h-[50vh] flex group hover:justify-center hover:items-center  shadow-md  rounded-3xl flex-shrink-0  relative overflow-hidden'>
         <div className="w-full h-full overflow-hidden">
         <img className='w-full h-full group-hover:scale-110 transition-all duration-500 object-cover object-top' src={character.img} alt="" />
         </div>
-     
        <div className='backdrop-blur-[1px] bg-[rgba(0,0,0,0.5);] transition-all duration-200 w-full h-full top-0 absolute opacity-0 group-hover:opacity-100'>
        <div className="w-full md:h-1/3 h-1/2 p-5 absolute opacity-0 transition-all duration-700  group-hover:opacity-100 group-hover:bottom-0 bottom-[-5%] ">
        <h3 className='text-sm text-[#D4F6FF] font-sans font-semibold'>{character.role}</h3>
--- Sarthak bhaiya/React/Bootcamp/class2/src/main.jsx
+++ Sarthak bhaiya/React/Bootcamp/class2/src/main.jsx
@@ -3,8 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { CharacterProvider } from './Context/CharacterContext';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+ <CharacterProvider>
     <App />
+  </CharacterProvider>,
   </StrictMode>,
 )
