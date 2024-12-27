bb211985f56be2adba27cd33fcb4947d16e890f5
Adit122022 <adity202212@gmail.com>
2024-12-27 11:19:43 +0530

    folder strcuture done but porject nedd to add

---
+++ Sarthak bhaiya/React/Bootcamp/class9/src/components/AddtoStory.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const AddtoStory = () => {
+    const [profile, setprofile] = useState('')
+    const [image, setImage] = useState('')
+    const [story, setStory] = useState('')
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    
+    }
+  return (
+    <div >
+        <form >
+            <label>Profile Name:</label>
+            <input type="text" value={profile} onChange={(e) => setprofile(e.target.value)} />
+            <label>Image URL:</label>
+            <input type="text" value={image} onChange={(e) => setImage(e.target.value)} />
+            <button type="submit" onClick={handleSubmit}>Add to Story</button>
+        </form>
+    </div>
+  )
+}
+
+export default AddtoStory
